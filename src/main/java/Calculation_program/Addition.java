b0f0e2e9dfaf93e2d897900e91cae54ad1fd40e1
akote <akote@entrata.com>
2022-03-28 14:21:20 +0530

    created progrma for git practice
    Please create new method

---
+++ src/main/java/Calculation_program/Addition.java
@@ -0,0 +1,32 @@
+package Calculation_program;
+
+
+public class Addition {
+
+    private int first_Number ;
+
+   private int second_number ;
+
+     final public int expectAnswerofAdditionMethood  =20;
+
+    public Addition() {
+    }
+
+    /** Method to return add the int number
+     *
+     * Auther : Amol kote
+       Written on : 28/03/2022
+    */
+
+
+    public int addition (int  first ,int second ){
+
+        first_Number = first ;
+        second_number = second ;
+
+        return first +second;
+    }
+
+
+
+}
---
+++ src/test/java/Addtion_test.java
@@ -0,0 +1,28 @@
+import Calculation_program.Addition;
+import static org.junit.Assert.*;
+import org.junit.Test;
+public class Addtion_test {
+
+    /** test to check addion method
+    
+    */
+
+@Test
+    public void additionTest() {
+
+        Addition ad= new Addition();
+    assertEquals(ad.expectAnswerofAdditionMethood, ad.addition(10,10));
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+}
